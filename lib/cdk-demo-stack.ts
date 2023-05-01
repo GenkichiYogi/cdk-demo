e09f87b71a297703f2bb2dd22d15b99f315dfbef
GenkichiYogi <gkt.yogi@mi-asahi.co.jp>
2023-05-01 16:29:46 +0900

    ハンズオンデモの完了

--- lib/cdk-demo-stack.ts
+++ lib/cdk-demo-stack.ts
@@ -1,5 +1,6 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkDemoStack extends cdk.Stack {
@@ -8,9 +9,21 @@ export class CdkDemoStack extends cdk.Stack {
 
     // The code that defines your stack goes here
 
+    new ec2.Vpc(this, 'mainVPC', {
+      maxAzs: 2,
+      subnetConfiguration: [
+        {
+        cidrMask: 24,
+        name: 'public-subnet',
+        subnetType: ec2.SubnetType.PUBLIC
+      }
+    ]
+    });
+
     // example resource
     // const queue = new sqs.Queue(this, 'CdkDemoQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
+
   }
 }
